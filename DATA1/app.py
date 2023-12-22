51331c5880feef6faf7e92bbc6c9ebd85c17e2d6
exthadhor <118166527+exthadhor@users.noreply.github.com>
2023-12-22 17:06:18 +0700

    Update app.py

--- DATA1/app.py
+++ DATA1/app.py
@@ -116,5 +116,5 @@ if __name__ == '__main__':
   # Load model yang telah ditraining
   model = load('dcp3.model')
 
-  app.run(host="0.0.0.0", port=4000, debug=True)
+  app.run(host="localhost", port=5000, debug=True)
   # app.run()
